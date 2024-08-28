dfce787ac27ed8cd3f549240293d272052cd555e
nimazzo <46820077+nimazzo@users.noreply.github.com>
2024-08-28 09:15:23 +0200

    Moved FPS counter to base app instead of duplicated across all tetrions

--- client/src/main/java/com/example/App.java
+++ client/src/main/java/com/example/App.java
@@ -5,19 +5,34 @@ import com.example.network.NOOPGameServerConnection;
 import com.example.simulation.Simulator;
 import com.example.ui.Tetrion;
 import com.example.worker.Worker;
+import javafx.animation.AnimationTimer;
 import javafx.application.Application;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 import java.io.DataInputStream;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.stream.Stream;
 
 public class App extends Application {
     public static final int NUM_PLAYERS = 1;
     private static final boolean SINGLE_PLAYER = NUM_PLAYERS == 1;
     private Simulator simulator;
+
+    // UI elements
+    private final List<Tetrion> tetrions = new ArrayList<>(NUM_PLAYERS);
+    private final Text fpsCounter = new Text();
+
+    // fps calculation
+    private long totalFrameTime = 0;
+    private int frameTimeIndex = 0;
+    private long last = System.nanoTime();
 
     public static void main(String[] args) {
         launch(args);
@@ -33,6 +48,13 @@ public class App extends Application {
 
         Worker.execute(this::connectToLobbyServer);
         stage.setOnCloseRequest(_ -> simulator.stopSimulating());
+
+        new AnimationTimer() {
+            @Override
+            public void handle(long now) {
+                redraw(now);
+            }
+        }.start();
     }
 
     private void connectToLobbyServer() {
@@ -54,12 +76,18 @@ public class App extends Application {
     }
 
     private Scene createContent() {
-        var tetrions = Stream.generate(Tetrion::new).limit(NUM_PLAYERS).toList();
+        Stream.generate(Tetrion::new).limit(NUM_PLAYERS).forEach(tetrions::add);
         simulator = new Simulator(tetrions);
 
-        var hbox = new HBox(10.0);
-        hbox.getChildren().addAll(tetrions);
-        var scene = new Scene(hbox);
+        var tetrionsBox = new HBox(10.0);
+        tetrionsBox.getChildren().addAll(tetrions);
+
+        var fpsBox = new HBox(new Text("FPS:"), fpsCounter);
+        fpsBox.setPadding(new Insets(10.0));
+
+        var root = new VBox(10.0, tetrionsBox, fpsBox);
+        var scene = new Scene(root);
+
         setupKeyboardInput(scene);
         return scene;
     }
@@ -88,5 +116,23 @@ public class App extends Application {
                 case ENTER -> simulator.setKeyState(6, false);
             }
         });
+    }
+
+    private void redraw(long now) {
+        tetrions.forEach(Tetrion::redraw);
+
+        totalFrameTime += now - last;
+        frameTimeIndex++;
+        last = now;
+
+        if (totalFrameTime >= 1_000_000_000) {
+            long averageFrameTime = totalFrameTime / frameTimeIndex;
+            long fps = 1_000_000_000 / averageFrameTime;
+            System.out.println("Num frames: " + frameTimeIndex + " total frame time: " + totalFrameTime + " average frame time: " + averageFrameTime);
+            fpsCounter.setText(String.format("%d", fps));
+
+            totalFrameTime = 0;
+            frameTimeIndex = 0;
+        }
     }
 }
